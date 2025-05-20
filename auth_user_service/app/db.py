16b4f6eb5c7360ee8531288d2dd28325eb719cb6
RonaldDaniel20 <rjacanamejoy@unal.edu.co>
2025-05-20 09:12:55 -0500

    Ajusta respuesta de endpoinst  a nuevos  atributos de la bd

--- auth_user_service/app/db.py
+++ auth_user_service/app/db.py
@@ -1,6 +1,9 @@
 import os 
 from sqlalchemy  import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 engine = create_engine(os.environ['DATABASE_URL'], echo = True)
