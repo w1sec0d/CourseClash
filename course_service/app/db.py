7f1d2e333c8e2372c4f2a60c2441e914b9f1fec1
RonaldDaniel20 <rjacanamejoy@unal.edu.co>
2025-05-21 16:33:24 -0500

    Añade configuracion base de datos e inicialización de aplicación

---
+++ course_service/app/db.py
@@ -0,0 +1,24 @@
+import os 
+from sqlalchemy  import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+
+#Configuración de looging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info("Initializing database connection...")
+engine = create_engine(os.environ['DATABASE_URL'], echo = True)
+logger.info(f"Connecting to database: {os.environ['DATABASE_URL']}")
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+logger.info("Database connection initialized successfully!")
+
+def get_db():
+    db = session()
+    try: 
+        yield db
+    finally: 
+        db.close()
--- course_service/app/main.py
+++ course_service/app/main.py
@@ -1,17 +1,41 @@
+# from fastapi import FastAPI
+# from . import models
+# from .database import engine
+# from .routers import user, course, activity
+
+# # Create database tables
+# models.Base.metadata.create_all(bind=engine)
+
+# app = FastAPI(
+#     title="Course Service",
+#     description="Course service for CourseClash",
+#     version="0.1.0",
+# )
+
+# # Include routers
+# app.include_router(course.router)
+# app.include_router(activity.router)
+
+import uvicorn
 from fastapi import FastAPI
-from . import models
-from .database import engine
-from .routers import user, course, activity
 
-# Create database tables
-models.Base.metadata.create_all(bind=engine)
 
+# Importacion de la base de datos
+from .db import get_db
+
+
+# Creación del microservicio de autenticación
 app = FastAPI(
-    title="Course Service",
-    description="Course service for CourseClash",
-    version="0.1.0",
-)
+    title="CourseClash Auth Service", 
+    description="API for CourseClash authentication and user management"
+    )
 
-# Include routers
-app.include_router(course.router)
-app.include_router(activity.router)
+
+
+
+# Rutas de servicios de los cursos
+
+
+# Punto de entrada del microservicio
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host = "0.0.0.0", port = 8001)
