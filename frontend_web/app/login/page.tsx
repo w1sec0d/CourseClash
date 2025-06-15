d68daf52d580de6a350e01107a7654aa4e24d88f
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-06-15 01:12:51 -0500

    Quita useState innecesario

--- frontend_web/app/login/page.tsx
+++ frontend_web/app/login/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
 import { useAuthApollo } from '@/lib/auth-context-apollo';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
